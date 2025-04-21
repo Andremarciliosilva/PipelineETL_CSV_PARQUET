1e94a31d4a7a6ea87edb22d8c8bb1754786c527f
Andre Silva <andremarciliosilva@gmail.com>
2025-04-21 12:10:43 -0300

    Atualização do arquivo pipeline.py com os novos métdoso criados

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,19 @@
 import classes as cls
 
-# Aramazenamento dos três arquivos .JSON em variáveis pra facilitar o manuseio.
-
-json1 = 'coleta_dia01.json'
-json2 = 'coleta_dia02.json'
-json3 = 'coleta_dia03.json'
-
-# Métodos da classe pra ler os arquivos .JSON, transformar pra Dataframe e concatenar os 3 DF's.
+# Métodos da classe Tratamentojson pra ler os arquivos .JSON, transformar pra Dataframe e concatenar os 3 DF's.
 
 tratamento_json = cls.Tratamentojson()
 tratamento_json.lerarquivos('coleta_dia01.json')
-tratamento_json.transformardf('coleta_dia03.json')
-tratamento_json.concatenardfs (json1, json2, json3)
+tratamento_json.transformardf('coleta_dia01.json')
+df_concat = tratamento_json.concatenardfs ('coleta_dia01.json', 'coleta_dia02.json', 'coleta_dia02.json')
+
+# Métodos da classe Transformarjson pra formatar os dados, agrupar por categoria, calcular vendas totais e filtrar vendas acima de R$ 10.000,00.
+
+transformacao_json = cls.Transformarjson()
+transformacao_json.formatardata(df_concat)
+transformacao_json.agruparcat(df_concat)
+transformacao_json.vendastotais(df_concat)
+transformacao_json.vendasup10mil(df_concat)
+
+decisao_saida = cls.Decisaosaida()
+decisao_saida.saidacsv('dados_concatenados.csv')
